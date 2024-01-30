f55b244b13d3b85b7f52df67b4e5e95dc24f1a13
chamalkaMarasinghe <chamalkamarasinghe11@gmail.com>
2024-01-31 03:05:50 +0530

    scrollable views

--- src/scenes/upcomingWeather/upcomingWeather.js
+++ src/scenes/upcomingWeather/upcomingWeather.js
@@ -8,6 +8,7 @@ import {
     FlatList,
     Animated,
     Easing,
+    ScrollView
 } from "react-native";
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
@@ -36,6 +37,55 @@ const DATA = [
         temp_max: 6,
         weather: "cloudy"
     },
+    {
+        time: "2024-08-30",
+        temp_min: 3,
+        temp_max: 6,
+        weather: "clear"
+    },
+    {
+        time: "2024-09-30",
+        temp_min: 3,
+        temp_max: 6,
+        weather: "rainy"
+    },
+    // ...
+    {
+        time: "2024-01-30",
+        temp_min: 3,
+        temp_max: 6,
+        weather: "clear"
+    },
+    {
+        time: "2024-02-30",
+        temp_min: 3,
+        temp_max: 6,
+        weather: "cloudy"
+    },
+    {
+        time: "2024-03-30",
+        temp_min: 3,
+        temp_max: 6,
+        weather: "rainy"
+    },
+    {
+        time: "2024-05-30",
+        temp_min: 3,
+        temp_max: 6,
+        weather: "cloudy"
+    },
+    {
+        time: "2024-08-30",
+        temp_min: 3,
+        temp_max: 6,
+        weather: "clear"
+    },
+    {
+        time: "2024-09-30",
+        temp_min: 3,
+        temp_max: 6,
+        weather: "rainy"
+    },
 ];
 
 const UpcomingWeather = () => {
@@ -59,40 +109,78 @@ const UpcomingWeather = () => {
 
     return(
         <SafeAreaView style={styles.container}>
-            <ImageBackground
-                source={require('../../../assets/images/weather.jpg')}
-                style={styles.bgImage}
-            >
-                <View style={styles.banner}>
-                    <Text style={styles.bannerText}>Upcoming Weather</Text>
-                </View>
-            </ImageBackground>
-            <FlatList
-                data={DATA}
-                renderItem={({item}) => (
-                    <View style={styles.card}>
-                        <View style={styles.leftBall}>
-                            {
-                                item.weather === 'sunny' ? <Icon name="sunny" size={40} style={{color: 'white'}}/> :
-                                item.weather === 'cloudy' ? <Icon name="cloud" size={40} style={{color: 'white'}}/> :
-                                item.weather === 'rainy' ? <Icon name="cloudy-snowing" size={40} style={{color: 'white'}}/> :
-                                <Icon name="sunny" size={40} style={{color: 'white'}}/>
-                            }
-                        </View>
-                        <View style={styles.details}>
-                            <Text style={styles.date}>{item.time}</Text>
-                            <Text style={styles.nature}>{item.weather}</Text>
-                            <Text style={styles.temp}>Maximum : {item.temp_max} Minimum : {item.temp_min}</Text>
-                        </View>
-                        <View style={styles.sync}>
-                            <Animated.View style={{ transform: [{ rotate: spin }] }}>
-                                <Icon name="sync" size={40}/>
-                            </Animated.View>
-                        </View>
+            <ScrollView>
+                <ImageBackground
+                    source={require('../../../assets/images/weather.jpg')}
+                    style={styles.bgImage}
+                >
+                    <View style={styles.banner}>
+                        <Text style={styles.bannerText}>Upcoming Weather</Text>
                     </View>
-                )}
-                contentContainerStyle={styles.list}
-            />
+                </ImageBackground>
+                <FlatList
+                    data={DATA}
+                    renderItem={({item}) => (
+                        <View style={styles.card}>
+                            <View style={styles.leftBall}>
+                                {
+                                    item.weather === 'sunny' ? <Icon name="sunny" size={40} style={{color: 'white'}}/> :
+                                    item.weather === 'cloudy' ? <Icon name="cloud" size={40} style={{color: 'white'}}/> :
+                                    item.weather === 'rainy' ? <Icon name="cloudy-snowing" size={40} style={{color: 'white'}}/> :
+                                    <Icon name="sunny" size={40} style={{color: 'white'}}/>
+                                }
+                            </View>
+                            <View style={styles.details}>
+                                <Text style={styles.date}>{item.time}</Text>
+                                <Text style={styles.nature}>{item.weather}</Text>
+                                <Text style={styles.temp}>Maximum : {item.temp_max} Minimum : {item.temp_min}</Text>
+                            </View>
+                            <View style={styles.sync}>
+                                <Animated.View style={{ transform: [{ rotate: spin }] }}>
+                                    <Icon name="sync" size={40}/>
+                                </Animated.View>
+                            </View>
+                        </View>
+                    )}
+                    style={styles.list}
+                    contentContainerStyle={styles.listContent}
+                />
+                <ImageBackground
+                    source={require('../../../assets/images/weather.jpg')}
+                    style={styles.bgImage}
+                >
+                    <View style={styles.banner}>
+                        <Text style={styles.bannerText}>outgoing Weather !</Text>
+                    </View>
+                </ImageBackground>
+                <FlatList
+                    data={DATA}
+                    renderItem={({item}) => (
+                        <View style={styles.card}>
+                            <View style={styles.leftBall}>
+                                {
+                                    item.weather === 'sunny' ? <Icon name="sunny" size={40} style={{color: 'white'}}/> :
+                                    item.weather === 'cloudy' ? <Icon name="cloud" size={40} style={{color: 'white'}}/> :
+                                    item.weather === 'rainy' ? <Icon name="cloudy-snowing" size={40} style={{color: 'white'}}/> :
+                                    <Icon name="sunny" size={40} style={{color: 'white'}}/>
+                                }
+                            </View>
+                            <View style={styles.details}>
+                                <Text style={styles.date}>{item.time}</Text>
+                                <Text style={styles.nature}>{item.weather}</Text>
+                                <Text style={styles.temp}>Maximum : {item.temp_max} Minimum : {item.temp_min}</Text>
+                            </View>
+                            <View style={styles.sync}>
+                                <Animated.View style={{ transform: [{ rotate: spin }] }}>
+                                    <Icon name="sync" size={40}/>
+                                </Animated.View>
+                            </View>
+                        </View>
+                    )}
+                    style={styles.list}
+                    contentContainerStyle={styles.listContent}
+                />
+            </ScrollView>
         </SafeAreaView>
     );
 };
@@ -109,6 +197,8 @@ const styles = StyleSheet.create({
 
     banner: {
         flex: 1,
+        paddingTop: "15%",
+        paddingBottom: "15%",
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: 'rgba(0, 0, 0, 0.6)'
@@ -123,8 +213,11 @@ const styles = StyleSheet.create({
 
     list: {
         flex: 1,
+        marginBottom: 5
+    },
+
+    listContent: {
         alignItems: 'center',
-        // backgroundColor: 'red'
     },
 
     card: {
@@ -147,7 +240,6 @@ const styles = StyleSheet.create({
     leftBall: {
         backgroundColor: '#2f610f',
         padding: 30,
-        // width: 95,
         marginLeft: -25,
         marginRight: 10,
         borderRadius: 50
@@ -174,7 +266,6 @@ const styles = StyleSheet.create({
     sync: {
         flex: 1,
         alignItems: 'flex-end',
-        // backgroundColor: 'red',
     }
 });
 
